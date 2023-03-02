1231b57a91ccce31161d6a09d3f6cab46f881e54
Maya Lira <mkzk.mail@gmail.com>
2023-03-02 18:13:51 -0300

    feat: implement library indexing

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from "@nestjs/common";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from "path";
+import { LibraryModule } from "./library/library.module";
 
 @Module({
-    imports: [],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: path.join(__dirname, "..", "library"),
+        }),
+        LibraryModule
+    ],
 })
 export class AppModule {}
---
+++ server/src/config.ts
@@ -0,0 +1,5 @@
+import * as path from "path";
+
+export const config = {
+    libraryDir: path.join(__dirname, "..", "library"),
+};
